82d2f5031bbfaa33463fe9bfb532b221c3fab211
kakaiking <kakaiphil@gmail.com>
2024-08-16 16:50:06 +0300

    3rd commit

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is running on port: ${PORT}`);
-})
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('WElcome to MERN')
+});
+
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+        console.log(`App is running on port: ${PORT}`);
+    });
+  }).catch((err) => {
+    console.log(err)
+  });

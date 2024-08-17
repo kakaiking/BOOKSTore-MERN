b5d898fd09f24aae83b85ec89483ca98f276a29c
kakaiking <kakaiphil@gmail.com>
2024-08-17 17:30:51 +0300

    7th commit

--- backend/index.js
+++ backend/index.js
@@ -15,14 +15,14 @@ app.use(express.json());
 //Option 1: Allow All Origins with Default of cors(*)
 app.use(cors());
 
-//Option 1: Allow Custom Origins
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type']
-  })
-)
+//Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type']
+//   })
+// )
 
 app.get('/', (request, response) => {
     console.log(request);
